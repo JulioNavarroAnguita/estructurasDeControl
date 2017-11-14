44c65f6cef6350c0755e490c38b5af535b6427ff
Julio Navarro <julio.navarro.anguita@gmail.com>
2017-11-14 11:40:54 +0100

    modified:   .gitignore 	modified:   teoria/BucleConBreak.java 	new file:   teoria/BuclesAnidados.java 	new file:   teoria/TablaDeMultiplicar.java 	new file:   teoria/TablaDeMultiplicar.txt 	new file:   teoria/doc/BucleConBreak.html 	new file:   teoria/doc/allclasses-frame.html 	new file:   teoria/doc/allclasses-noframe.html 	new file:   teoria/doc/constant-values.html 	new file:   teoria/doc/deprecated-list.html 	new file:   teoria/doc/help-doc.html 	new file:   teoria/doc/index-all.html 	new file:   teoria/doc/index.html 	new file:   teoria/doc/overview-tree.html 	new file:   teoria/doc/package-frame.html 	new file:   teoria/doc/package-list 	new file:   teoria/doc/package-summary.html 	new file:   teoria/doc/package-tree.html 	new file:   teoria/doc/script.js 	new file:   teoria/doc/stylesheet.css

--- teoria/BucleConBreak.java
+++ teoria/BucleConBreak.java
@@ -1,13 +1,20 @@
+/**Fichero de datos BucleConBreak.java
+ * Ejemplo de funcionamiento
+ * de programa capaz de 
+ * calcular si es primo o no
+ * @author Julio Navarro
+ * @version 1.0*/
+
 public class BucleConBreak {
     public static void main (String[] args) {
 
-        long posibleNumeroPrimo =  13139;
+        long posibleNumeroPrimo =  997;
         boolean resultado = false;
         //for (int divisor = 2 ; divisor < posibleNumeroPrimo ; divisor++ ) {
         //de la mitad del posibleNuemeroPrimo hasta posibleNumeroPrimos 
         //NUNCA existirá un posible divisor, se mejora el algoritmo si
         // ponemos divisor < posibleNumeroPrimo / 2
-        for (long divisor = 2 ; divisor < posibleNumeroPrimo / 2 ; divisor++ ) {
+        for (long divisor = 2 ; divisor <= posibleNumeroPrimo / 2 ; divisor++ ) {
             resultado = posibleNumeroPrimo % divisor == 0;
             //if (resultado == true) {
             if ( resultado ) {
@@ -16,9 +23,30 @@ public class BucleConBreak {
             }
            // System.out.println(posibleNumeroPrimo + "  " + divisor + "  " + resultado); 
         }
-        System.out.println("¿Es nº primo " + posibleNumeroPrimo + "? " + !resultado);
+        System.out.println("Es numero primo " + posibleNumeroPrimo + " ? " + !resultado);
+        System.out.println("Es numero primo " + 86743 + " ? " + esNumeroPrimo(86743));
+        System.out.println("Es numero primo " + 997 + " ? " + esNumeroPrimo(997));
+
 
         // System.out.println("¿Es nº primo " + posibleNumeroPrimo + "? " + esNumeroPrimo (posibleNumeroPrimo ) );
     }
-    // public static boolean esNumeroPrimo (long numero)  
+    /**Método que nos devuelve si un número
+     * es primo o no, pasando este parámetro
+     * @param numero el dígito que se comprobará si es primo o no
+     * @return true o false dependiendo si es o no es número primo
+     * */
+     
+    public static boolean esNumeroPrimo (long numero) {
+		
+		boolean resultado = false;
+		
+		for (long divisor = 2 ; divisor <= numero / 2 ; divisor++ ) {
+            resultado = numero % divisor == 0;
+            if ( resultado ) {
+                break;
+            } 
+        }
+        return !resultado;
+	} 
+	
 }
---
+++ teoria/BuclesAnidados.java
@@ -0,0 +1,31 @@
+public class BuclesAnidados {
+	
+	public static void main (String[] args) {
+		
+		for (int tabla1 = 1; tabla1 < 11; tabla1++) {
+			for(int tabla2 = 2; tabla2 < 11; tabla2++) {
+				for(int tabla3 = 3; tabla3 < 11; tabla3++) {
+					for(int tabla4 = 4; tabla4 < 11; tabla4++) {
+						for(int tabla5 = 5; tabla5 < 11; tabla5++) {
+							for(int tabla6 = 6; tabla6 < 11; tabla6++) {
+								for(int tabla7 = 7; tabla7 < 11; tabla7++) {
+									for(int tabla8 = 8; tabla8 < 11; tabla8++) {
+										for(int tabla9 = 9; tabla9 < 11; tabla9++) {	
+											for(int tabla10 = 10; tabla5 < 10; tabla10++) {
+												System.out.println("Tabla del 1: " + tabla1 + " ------ Tabla del 2: " + j + " ------ Valor de k: " + k);
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		
+		}
+		
+	}
+	
+}
+
