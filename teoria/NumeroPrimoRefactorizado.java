8514889f5f9462767f7e25bf3441a9a2f4afb79c
Julio Navarro <julio.navarro.anguita@gmail.com>
2017-11-16 17:25:04 +0100

    5º Commit 	modified:   teoria/NumeroPrimoRefactorizado.java

--- teoria/NumeroPrimoRefactorizado.java
+++ teoria/NumeroPrimoRefactorizado.java
@@ -1,41 +1,42 @@
 
 public class NumeroPrimoRefactorizado {
-	public static void main (String[] args) {
-	
-		long posibleNumeroPrimo = 7;
-		//84979;
-		boolean verificacionDivisible235 = verificarDivisibilidad2o3o5 (posibleNumeroPrimo);
-			System.out.println("Es divisible entre 2, 3 o 5? " + verificacionDivisible235);
-		int calcularEnteroRaizCuadrada = valorEnteroRaizCuadrada (posibleNumeroPrimo);
-			System.out.println("Raiz cuadrada de " + posibleNumeroPrimo + " es: " + calcularEnteroRaizCuadrada);
-		boolean verificacionRaizCuadrada = calcularEnteroRaizCuadrada * calcularEnteroRaizCuadrada == posibleNumeroPrimo;
-			System.out.println("La raiz cuadrada de " + posibleNumeroPrimo + ": " + calcularEnteroRaizCuadrada + 
-			" al ser multiplicado por si mismo, es igual que " + posibleNumeroPrimo + " ? " + verificacionRaizCuadrada);
-		boolean verificacionNumeroPrimo = esNumeroPrimo(posibleNumeroPrimo);
-			System.out.println("Es entonces " + posibleNumeroPrimo + " un numero primo? " + verificacionNumeroPrimo);
-		boolean compuesto = verificacionDivisible235 || verificacionNumeroPrimo || verificacionRaizCuadrada;
-			System.out.println(posibleNumeroPrimo + " es compuesto? " + compuesto);	
+    public static void main(String[] args){
+        
+        long posibleNumeroPrimo = 98764321261L;
+        //long posibleNumeroPrimo = 7;
 
-	}
-
-	public static boolean verificarDivisibilidad2o3o5 (long numero) {
-		return numero % 2 == 0 || numero % 3 == 0 || numero % 5 == 0;
-	}
-
-	public static int valorEnteroRaizCuadrada (long numero) {
-		return (int) Math.sqrt(numero);
-	}
-
-	public static boolean esNumeroPrimo (long numero) {
-		boolean resultado = false;
-		for (long divisor = 7 ; divisor < numero / 2 ; divisor++ ) {
-            resultado = numero % divisor == 0;
-            if (resultado) {
-				System.out.println("divisor" + divisor);
-                break;
-            } 
+        if ( verificarDivisibilidad2o3o5(posibleNumeroPrimo) ) {
+            System.out.println( posibleNumeroPrimo + " es compuesto" );
+            System.exit(1);
         }
-        return resultado;
-	} 
+
+        long valorEnteroRaizCuadrada = calcularEnteroRaizCuadrada(posibleNumeroPrimo);
+        boolean segundaVerificacion = valorEnteroRaizCuadrada * valorEnteroRaizCuadrada == posibleNumeroPrimo;
+
+
+        System.out.println( posibleNumeroPrimo + " ¿es compuesto? " + ( segundaVerificacion || verificarDivisibilidadPrimos(valorEnteroRaizCuadrada)));
+
+    }
+
+
+    public static boolean verificarDivisibilidad2o3o5 ( long numero ) {
+        return  numero % 2 == 0 || numero % 3 == 0 || numero % 5 == 0 ;        
+    }
+    
+    public static long calcularEnteroRaizCuadrada( long numero ) {
+        return (long) Math.sqrt(numero);
+    }
+
+    public static boolean verificarDivisibilidadPrimos( long numero ) {
+         boolean resultado = false;
+         for (long divisor = 7 ; divisor < numero / 2 ; divisor++ ) {
+            resultado = numero % divisor == 0;
+            if ( resultado ) {
+                break;
+            }
+         }
+         return resultado; 
+    }
+
 
 }
